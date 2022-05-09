f6cbeb1303f93afdc51a65667e0b5ab67686d368
梁轩伟 <xuanwei.liang@longbridge.sg>
2022-05-09 16:19:24 +0800

    upload quote examples

--- examples/push_broker.py
+++ examples/push_broker.py
@@ -1,16 +1,16 @@
-# 订阅行情数据
-# https://open.longbridgeapp.com/docs/quote/subscribe/subscribe
+# 实时经纪队列推送
+# https://open.longbridgeapp.com/docs/quote/push/push-brokers
 import os
 import time
 from longbridge.http import Auth, Config, HttpClient
 from longbridge.ws import ReadyState, WsCallback, WsClient
 # Protobuf 变量定义参见：https://github.com/longbridgeapp/openapi-protobufs/blob/main/quote/api.proto
-from longbridge.proto.quote_pb2 import (Command, PushQuote, SubscribeRequest, SubscriptionResponse, SubType)
+from longbridge.proto.quote_pb2 import (Command, PushBrokers, SubscribeRequest, SubscriptionResponse, SubType)
 
 class MyWsCallback(WsCallback):
     def on_push(self, command: int, body: bytes):
-        if command == Command.PushQuoteData:
-            quote = PushQuote()
+        if command == Command.PushBrokersData:
+            quote = PushBrokers()
             quote.ParseFromString(body)
             print(f"quote-> {quote}")
         else:
@@ -31,7 +31,7 @@ ws = WsClient("wss://openapi-quote.longbridge.global", http, MyWsCallback())
 #
 # 运行前请访问 “开发者中心“ 确保账户有正确的行情权限。
 # 如没有开通行情权限，可以通过 "长桥" 手机客户端，并进入 “我的 - 我的行情 - 行情商城“ 购买开通行情权限。
-req = SubscribeRequest(symbol=["700.HK", "AAPL.US"], sub_type=[SubType.QUOTE], is_first_push=True)
+req = SubscribeRequest(symbol=["700.HK"], sub_type=[SubType.BROKERS], is_first_push=True)
 result = ws.send_request(Command.Subscribe, req.SerializeToString())
 resp = SubscriptionResponse()
 resp.ParseFromString(result)
--- examples/subscription.py
+++ examples/subscription.py
@@ -1,11 +1,11 @@
-# 订阅行情数据
-# https://open.longbridgeapp.com/docs/quote/subscribe/subscribe
+# 获取已订阅标的行情
+# https://open.longbridgeapp.com/docs/quote/subscribe/subscription
 import os
 import time
 from longbridge.http import Auth, Config, HttpClient
 from longbridge.ws import ReadyState, WsCallback, WsClient
 # Protobuf 变量定义参见：https://github.com/longbridgeapp/openapi-protobufs/blob/main/quote/api.proto
-from longbridge.proto.quote_pb2 import (Command, PushQuote, SubscribeRequest, SubscriptionResponse, SubType)
+from longbridge.proto.quote_pb2 import (Command, PushQuote, SubscribeRequest, SubscriptionResponse, SubType, SubscriptionRequest, UnsubscribeRequest, UnsubscribeResponse)
 
 class MyWsCallback(WsCallback):
     def on_push(self, command: int, body: bytes):
@@ -31,13 +31,37 @@ ws = WsClient("wss://openapi-quote.longbridge.global", http, MyWsCallback())
 #
 # 运行前请访问 “开发者中心“ 确保账户有正确的行情权限。
 # 如没有开通行情权限，可以通过 "长桥" 手机客户端，并进入 “我的 - 我的行情 - 行情商城“ 购买开通行情权限。
-req = SubscribeRequest(symbol=["700.HK", "AAPL.US"], sub_type=[SubType.QUOTE], is_first_push=True)
+
+#订阅标的
+req = SubscribeRequest(symbol=["700.HK", "AAPL.US"], sub_type=[SubType.QUOTE], is_first_push=False)
 result = ws.send_request(Command.Subscribe, req.SerializeToString())
 resp = SubscriptionResponse()
 resp.ParseFromString(result)
 
 print(f"Subscribed symbol:\n\n {resp.sub_list}")
 
-print("\n\nWaiting for push...\nPress [Ctrl + c] to quit.")
-while True:
-    time.sleep(10)
+#取消订阅
+req = UnsubscribeRequest(symbol=["700.HK"], unsub_all=True)
+result = ws.send_request(Command.Unsubscribe, req.SerializeToString())
+
+#查询已订阅标的
+req = SubscriptionRequest()
+result = ws.send_request(Command.Subscription, req.SerializeToString())
+resp = SubscriptionResponse()
+resp.ParseFromString(result)
+
+print("\n")
+print(f"Subscribed symbol:\n\n {resp.sub_list}")
+
+#取消订阅
+req = UnsubscribeRequest(unsub_all=True)
+result = ws.send_request(Command.Unsubscribe, req.SerializeToString())
+
+#查询已订阅标的
+req = SubscriptionRequest()
+result = ws.send_request(Command.Subscription, req.SerializeToString())
+resp = SubscriptionResponse()
+resp.ParseFromString(result)
+
+print("\n")
+print(f"Subscribed symbol:\n\n {resp.sub_list}")
